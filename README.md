0ea5246be8b5b5c25f67d0d10df34ac1a8df77d0
Jan91 <jan@bolmer.de>
2017-07-18 15:53:50 -0400

    small changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 MCMC sampler for fitting X-shooter spectra (using PyMC2) with HI, H2 and
 other lines
+
+
+Under Construction - I am learning Git here ;)
+
--- bin/sns_plots.py
+++ bin/sns_plots.py
@@ -8,7 +8,7 @@ from pylab import *
 
 import seaborn as sns
 
-def sns_pair_plot(var_list, file):
+def sns_pair_plot(var_list, file, redshift):
 
 	data = pd.read_pickle(file)
 	
@@ -27,7 +27,7 @@ def sns_pair_plot(var_list, file):
 	g.savefig("H2_pairplot.pdf")
 
 
-def sns_H2vib_plot(var_list, file):
+def sns_H2vib_plot(var_list, file, redshift):
 
 	var_list = var_list
 
@@ -38,8 +38,10 @@ def sns_H2vib_plot(var_list, file):
 	data_f = {}
 
 	for var in var_list:
-		if not var.startswith("B_"):
+		if not var.startswith("B_") and not var.startswith("A_"):
 			data_f[var] = data[var][0]
+		if var.startswith("A_"):
+			data_f[var] = data[var][0] + redshift
 
 	df = pd.DataFrame(data_f)
 
